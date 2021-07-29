0a20d5fdba564ff26fd144edc4f56a507f927e71
TNyawara <Tiffany.Nyawara@strathmore.edu>
2021-07-29 10:56:02 +0300

    Animator Controller changes
    Animator Controller changes

---
+++ Assets/BunnyAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyAnimator : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+anim.Play("Running");
+}
+if(Input.GetKey(KeyCode.A)){
+anim.Play("idle");
+}
+if(Input.GetKey(KeyCode.S)){
+anim.Play("Dead");
+}
+//if(Input.GetKey(KeyCode.D)){
+anim.Play("0-idle_agressive");
+//}
+    }
+}
