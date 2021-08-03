ade4cb2044a22d37c5b245b28d8f490933689055
TNyawara <Tiffany.Nyawara@strathmore.edu>
2021-08-03 15:11:14 +0300

    Movement and Camera switching

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Follower : MonoBehaviour
 {
+    public Transform target;//This is the transform property of the target = Sphere
+    public Vector3 offset; //This is offset dimensions with respect to X,Y and Z
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class Follower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //The new position of the camera is the same as that of the sphere BUT with some distance
+    //in between (offset)
+    transform.position = target.position +offset;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int speed=30;
-    bool isMoving=false;
-    public rigidbody rabbit;
+    //public int speed=30;
+    //bool isMoving=false;
+    public Rigidbody rabbit;
+
+
+    public Camera MainCamera;
+    public Camera AerialCamera;
+    public Camera SideCamera;
     void Start()
     {
         
@@ -15,32 +20,40 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
      void Update() {
-        if (isMoving) {
-            return;
-        }
+        //if (isMoving) {
+         //   return;
+        //}
 
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            rabbit.transform.Translate(0f,0f,0.5f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+           rabbit.transform.Translate(0f,0f,-0.5f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rabbit.transform.Translate(-0.5f,0f,0f);
+        }
         if (Input.GetKey(KeyCode.RightArrow)) {
-            StartCoroutine(Roll(Vector3.right));
+            rabbit.transform.Translate(0.5f,0f,0f);
         }
-else if (Input.GetKey(KeyCode.LeftArrow)) {
-            StartCoroutine(Roll(Vector3.left));
+        if(Input.GetKey(KeyCode.J)){
+            MainCamera.enabled = false;
+            AerialCamera.enabled = true;
+            SideCamera.enabled = false;
+        
+        }
+        if(Input.GetKey(KeyCode.K)){
+            MainCamera.enabled = true;
+            AerialCamera.enabled = false;
+            SideCamera.enabled = false;
+        }
+        if(Input.GetKey(KeyCode.L)){
+            MainCamera.enabled = false;
+            AerialCamera.enabled = false;
+            SideCamera.enabled = true;
         }
     }
 
-    IEnumerator Roll(Vector3 direction) {
-        isMoving = true;
-
-        float remainingAngle = 90;
-        Vector3 rotationCenter = transform.position + direction / 2 + Vector3.down / 2;
-        Vector3 rotationAxis = Vector3.Cross(Vector3.up, direction);
-
-        while (remainingAngle > 0) {
-            float rotationAngle = Mathf.Min(Time.deltaTime * speed, remainingAngle);
-            transform.RotateAround(rotationCenter, rotationAxis, rotationAngle);
-            remainingAngle -= rotationAngle;
-            yield return null;
-        }
-
-        isMoving = false;
-    }
+  
+   
 }
