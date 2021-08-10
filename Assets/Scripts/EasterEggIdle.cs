052b6a645c61bb2528f012530812c1534a3a706a
TriadSyndicate <38569368+TriadSyndicate@users.noreply.github.com>
2021-08-10 03:29:20 +0300

    Rotator Script
    First rotator script to be added to other assets, fruits, gold coins etc

---
+++ Assets/Scripts/EasterEggIdle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggIdle : MonoBehaviour
+{
+    // Start is called before the first frame update
+            private readonly float FullRotation = 360f;
+        [SerializeField]
+        private float speed = 1f;
+        private float currentRotation = 0f;
+
+        public event EventHandler FullyRotated;
+        private bool IsFullRotation
+        {
+            get { return Mathf.Abs(currentRotation) >= FullRotation; }
+        } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        RaiseEventOnFullRotation();
+        ResetRotationOnFullRotation();
+    }
+    private void Rotate()
+    {
+        transform.Rotate(0, speed, 0);
+        currentRotation += speed;
+    }
+    private void RaiseEventOnFullRotation()
+    {
+    if (IsFullRotation)
+    {
+        if (FullyRotated!=null)
+        {
+            FullyRotated(this, new EventArgs());
+        }
+    }
+    }
+    private void ResetRotationOnFullRotation()
+    {
+        if (IsFullRotation)
+        {
+        currentRotation -= FullRotation * Mathf.Sign(currentRotation);
+        }
+    }
+}
