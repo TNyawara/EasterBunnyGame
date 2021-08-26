01204302cdcb4a98f94c28bc0de056369842aeb5
TriadSyndicate <38569368+TriadSyndicate@users.noreply.github.com>
2021-08-26 08:43:49 +0300

    scene generator

--- Assets/Scripts/Environment/EndlessGenerator.cs
+++ Assets/Scripts/Environment/EndlessGenerator.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EndlessGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public GameObject[] section;
+    public int zPos = 48;
+    public bool creatingSection = false;
+    public int secNum;
     // Update is called once per frame
     void Update()
     {
-        
+        if (creatingSection == false)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+    IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(section[secNum], new Vector3(0,0,zPos), Quaternion.identity);
+        zPos += 200;
+        yield return new WaitForSeconds(5);
+        creatingSection = false;
     }
 }
