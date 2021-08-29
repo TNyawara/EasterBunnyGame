9682c33e2a43050b0c437ebb83d3a92b1b2686aa
TNyawara <Tiffany.Nyawara@strathmore.edu>
2021-08-29 22:35:56 +0300

    Added background music and victory

--- Assets/Scripts/LevelShift.cs
+++ Assets/Scripts/LevelShift.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class LevelShift : MonoBehaviour
 {
     public Level level;
+    public audio audio;
+    public AudioClip levelsound;
     private void OnTriggerEnter(Collider other)
     {
+    
         int current_level = level.getLevel();
         int next_level = current_level + 1;
         level.setLevel(next_level);
+        audio.Sound(levelsound);
     }
 }
---
+++ Assets/audio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+  public AudioSource BGM;
+    
+    public void Sound(AudioClip Music){
+    BGM.clip=Music;
+    BGM.Play();
+    
+    }
+}
