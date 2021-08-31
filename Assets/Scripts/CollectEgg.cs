b28debf3a6aec912cd26d8e4ad8d90181c826e49
TNyawara <Tiffany.Nyawara@strathmore.edu>
2021-08-31 20:50:09 +0300

    egg collide

---
+++ Assets/Scripts/CollectEgg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectEgg : MonoBehaviour
+{
+  // public AudioSource coinFX;
+   // public score score;
+    void OnTriggerEnter(Collider other)
+    {
+        
+            //Debug.Log("Collide");
+            //coinFX.Play();
+            this.gameObject.SetActive(false);
+            //score.setScore(5);
+    }
+}
