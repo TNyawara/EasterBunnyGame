c8e47da9305c32580c3d1307af436183f50a83a9
TNyawara <Tiffany.Nyawara@strathmore.edu>
2021-08-31 21:50:02 +0300

    egg instantiation

--- Assets/Scripts/CollectEgg.cs
+++ Assets/Scripts/CollectEgg.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CollectEgg : MonoBehaviour
 {
   // public AudioSource coinFX;
-   // public score score;
+   public score score;
     void OnTriggerEnter(Collider other)
     {
         
             //Debug.Log("Collide");
             //coinFX.Play();
             this.gameObject.SetActive(false);
-            //score.setScore(5);
+            score.setScore(1);
     }
 }
---
+++ Assets/Scripts/EggScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EggScore : MonoBehaviour
+{
+     public TMP_Text eggscore_text;
+    public int current_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_score = 0;
+        eggscore_text.text = current_score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setScore(int plus)
+    {
+        current_score += plus;
+        eggscore_text.text = current_score.ToString();
+    }
+}
